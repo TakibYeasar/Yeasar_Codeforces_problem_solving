fd86b32f400428ceb2bea700467088a64e07434a
Takib Yeasar <mdtakibyeasar@gmail.com>
2024-07-13 21:32:03 +0600

    aaa

---
+++ Binary Search/codeforces_1985F.cpp
@@ -0,0 +1,31 @@
+
+// Problem link ===>> https://codeforces.com/problemset/problem/1985/F
+// submission link ===>>
+
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll h, n;
+        cin >>h>> n;
+
+        vector<ll> a(n);
+        for (ll i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+        vector<ll> c(n);
+        for (ll i = 0; i < n; i++){
+            cin >> c[i];
+        }
+
+        
+    }
+    return 0;
+}
---
+++ Binary Search/codeforces_1985F.py
@@ -0,0 +1,13 @@
+
+# Problem link ===>> https://codeforces.com/problemset/problem/1985/F
+
+
+def main():
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        
+
+
+if __name__ == "__main__":
+    main()

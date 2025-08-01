dd13c663a157b9880dfcc1e7c0a87e77a491adb9
Takib Yeasar <mdtakibyeasar@gmail.com>
2025-08-01 22:23:31 +0600

    solved

--- Graph Theory/codeforces_2114E.cpp
+++ Graph Theory/codeforces_2114E.cpp
@@ -1,6 +1,6 @@
 
 // Problem link ===>> https://codeforces.com/problemset/problem/2114/E
-// submission link ===>>
+// submission link ===>> https://codeforces.com/contest/2114/submission/331737891
 
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
@@ -18,6 +18,23 @@ using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statisti
 template <typename T>
 using ordered_set_mul = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
+const int MAXN = 2e5 + 5;
+vector<int> gr[MAXN];
+vector<ll> arr(MAXN);
+vector<ll> res(MAXN);
+
+void dfs(int v, int p, ll mini, ll maxi)
+{
+    res[v] = max(arr[v], -mini + arr[v]);
+    ll new_min = min(arr[v], -maxi + arr[v]);
+    for (int u : gr[v])
+    {
+        if (u == p)
+            continue;
+        dfs(u, v, new_min, res[v]);
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -31,7 +48,29 @@ int main()
         ll n;
         cin >> n;
 
-        
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> arr[i];
+            gr[i].clear();
+        }
+
+        for (int i = 0; i < n - 1; ++i)
+        {
+            int u, v;
+            cin >> u >> v;
+            --u, --v;
+            gr[u].push_back(v);
+            gr[v].push_back(u);
+        }
+
+        res.assign(n, 0);
+        dfs(0, -1, 0, 0);
+
+        for (int i = 0; i < n; ++i)
+        {
+            cout << res[i] << " ";
+        }
+        cout << endl;
     }
     return 0;
 }
--- Graph Theory/codeforces_2114E.py
+++ Graph Theory/codeforces_2114E.py
@@ -10,15 +10,51 @@ from functools import lru_cache, reduce
 from itertools import permutations, combinations, accumulate
 from typing import List, Tuple, Dict, Set, Optional, Union, Any
 
+sys.setrecursionlimit(400_000)
+
 MOD = 10**9 + 7
 INF = float('inf')
 input = sys.stdin.read
 print = sys.stdout.write
 
+
 def main():
-    t = int(input())
+    data = input().split()
+    idx = 0
+    t = int(data[idx])
+    idx += 1
+
+    results = []
+
     for _ in range(t):
-        n = int(input())
+        n = int(data[idx])
+        idx += 1
+
+        arr = list(map(int, data[idx:idx + n]))
+        idx += n
+
+        gr = [[] for _ in range(n)]
+        for _ in range(n - 1):
+            u = int(data[idx]) - 1
+            v = int(data[idx + 1]) - 1
+            gr[u].append(v)
+            gr[v].append(u)
+            idx += 2
+
+        res = [0] * n
+
+        def dfs(v: int, p: int, mini: int, maxi: int):
+            res[v] = max(arr[v], -mini + arr[v])
+            new_mini = min(arr[v], -maxi + arr[v])
+            for u in gr[v]:
+                if u != p:
+                    dfs(u, v, new_mini, res[v])
+
+        dfs(0, -1, 0, 0)
+        results.append(" ".join(map(str, res)))
+
+    print("\n".join(results))
+
 
 if __name__ == "__main__":
     main()
